85443b12db0ec9f0a6470baaf2ba590ab6de1f57
Jashanuu <jsingh303@caledonian.ac.uk>
2025-07-30 04:42:22 +0000

    Added CI test print line

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -1,35 +1,38 @@
-// Java Program to demonstrates the Number guessing game 
-import java.util.Scanner; 
+import java.util.Scanner;
 
-public class NumberGuessing { 
-    public static void guessingNumberGame() { 
-        Scanner sc = new Scanner(System.in); 
-        int number = 1 + (int)(100 * Math.random()); 
-        int K = 5; 
+public class NumberGuessing {
 
-        System.out.println("A number is chosen between 1 and 100."); 
-        System.out.println("You have " + K + " attempts to guess the correct number."); 
+    public static void guessingNumberGame() {
+        Scanner sc = new Scanner(System.in);
+        int number = 1 + (int)(100 * Math.random());
+        int K = 5;
 
-        for (int i = 0; i < K; i++) { 
-            System.out.print("Enter your guess: "); 
-            int guess = sc.nextInt(); 
+        System.out.println("A number is chosen between 1 and 100.");
+        System.out.println("You have " + K + " attempts to guess the correct number.");
 
-            if (guess == number) { 
-                System.out.println("Congratulations! You guessed the correct number."); 
-                sc.close(); 
-                return; 
-            } else if (guess < number) { 
-                System.out.println("The number is greater than " + guess); 
-            } else { 
-                System.out.println("The number is less than " + guess); 
-            } 
-        } 
+        for (int i = 0; i < K; i++) {
+            System.out.print("Enter your guess: ");
+            int guess = sc.nextInt();
 
-        System.out.println("You've exhausted all attempts. The correct number was: " + number); 
-        sc.close(); 
-    } 
+            if (guess == number) {
+                System.out.println("Congratulations! You guessed the correct number.");
+                sc.close();
+                return;
+            } else if (guess < number) {
+                System.out.println("The number is greater than " + guess);
+            } else {
+                System.out.println("The number is less than " + guess);
+            }
+        }
 
-    public static void main(String[] args) { 
-        guessingNumberGame(); 
-    } 
+        System.out.println("You've exhausted all attempts. The correct number was: " + number);
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        // 👇 This line is for CI pipeline test
+        System.out.println("CI Test run!");
+
+        guessingNumberGame();
+    }
 }
